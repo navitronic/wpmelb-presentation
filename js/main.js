ae08f7ac2b6b2647b38a64618c1286fef460edcf
Adrian Palmer <adrian@navitronic.co.uk>
2014-04-14 22:56:19 +1000

    Updated files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+jQuery(function($) {
+    $('h1,h2,h3,li,p').each(function() {
+        $(this).html($(this).html().replace(/\s([^\s<]+)\s*$/,'&nbsp;$1'));
+    });
+});
+
 bespoke.horizontal.from('article', {
   bullets: 'li, .bullet',
   scale: true,
